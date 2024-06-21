b4c36ec5bddba0ada2092bc16be8a88bcdfb7130
Robert Tanase <robert.tanase97@gmail.com>
2024-06-21 14:33:18 +0300

    Version 1.6.5

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/crowdconnected/crowdconnected-shared-ios.git",
-                 from: "1.6.4")
+                 from: "1.6.5")
     ],
     targets: [
         .binaryTarget(name: "CrowdConnectedIPS",
