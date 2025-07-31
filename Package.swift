565f76a493bf99953ca0b5e36080fd35e16f6d51
CrowdConnected CI <ci@crowdconnected.com>
2025-07-31 20:20:33 +0100

    Version 2.1.3-beta.8

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/kin2025cc/crowdconnected-shared-ios.git",
-                 exact: "2.1.3-beta.7")
+                 exact: "2.1.3-beta.8")
     ],
     targets: [
         .binaryTarget(name: "CrowdConnectedIPS",
